f8a4c319da1b3569fc58ec9fa7b1d5e6331fa664
Jasonnzzz <jasonchandra82010@gmail.com>
2024-05-17 13:58:28 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ document.querySelector('#search').onclick =(e)=>{
     e.preventDefault();
 };
 document.querySelector('#shopping-cart-button').onclick=()=>{
-    ShoppingCart.classList.toggle('active')
+    ShoppingCart.classList.toggle('active');
     e.preventDefault();
 }
 
@@ -54,3 +54,74 @@ window.onclick = (e) => {
         modal.style.display = 'none';
     }
 }
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('products', () => ({
+        items: [
+        { id: 1, name: 'Robusta Brazil', img: '1.jpg', price: 20000 },
+        { id: 2, name: 'Arabica Blend', img: '1.jpg', price: 25000 },
+        { id: 3, name: 'Primo Passo', img: '1.jpg', price: 30000 },
+        { id: 4, name: 'Cappuchino', img: '1.jpg', price: 40000 },
+        { id: 5, name: 'Chocolatte', img: '1.jpg', price: 10000 },
+        ],
+    }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            if(!cartItem) {
+                this.items.push({...newItem, quantity: 1, total: newItem.price });
+                this.quantity++;
+                this.total += newItem.price;
+
+            } else {
+                this.items = this.items.map((item) => {
+                    if(item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                })            
+            }
+        },
+        remove(id) {
+            const cartItem = this.items.find((item) => item.id === id);
+            if(cartItem.quantity > 1) {
+                this.items = this.items.map((item) => {
+                    if(item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item
+                    }
+                })
+            } else if (cartItem.quantity === 1) {
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price
+            }
+        }
+    })
+});
+
+// mata uang
+
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+}).format(number);
+
+};
